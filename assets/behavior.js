5edd1665e9cb361882b863b0fee4579818465830
Tectual <arash@tectual.com.au>
2015-02-14 19:02:19 +1100

    new doc

--- assets/behavior.js
+++ assets/behavior.js
@@ -164,6 +164,14 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
           "type": "heading",
           "data": {
             "level": 2,
+            "title": "Get &amp; Update a document",
+            "slug": "get-amp-update-a-document"
+          },
+          "depth": 2
+        }, {
+          "type": "heading",
+          "data": {
+            "level": 2,
             "title": "Create or Replace a document",
             "slug": "create-or-replace-a-document"
           },
